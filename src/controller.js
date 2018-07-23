5681584de85b42c62c818898b22e0b6d44b22461
PSyCOhTOa <thomsonar13@GMAIL.COM>
2018-07-23 14:25:52 +1200

    Created and Initialised Files for Construction
    Implemented Index, Controller and Test files for JS Classes, and starting to display them

---
+++ src/controller.js
@@ -0,0 +1,6 @@
+class Controller {// eslint-disable-line no-unused-vars
+    static setup () {
+      let IntroToProgramming = new Test('BCPR101 - Intro To Programming')
+      return IntroToProgramming
+    }
+}
