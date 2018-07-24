10967352f2a51edbe1a7ef1066967d7d3e8866a1
PSyCOhTOa <thomsonar13@GMAIL.COM>
2018-07-24 12:42:30 +1200

    Added All Additional Files Remaining
    Remaining Class Files Added

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,9 @@
 class Controller {// eslint-disable-line no-unused-vars
     static setup () {
-      let IntroToProgramming = new Test('BCPR101 - Intro To Programming')
+      let IntroToProgramming = new Course('BCPR101 - Intro To Programming')
+
+      //Add all questions here
+
       return IntroToProgramming
     }
 }
